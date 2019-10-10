d8749fe73115446fbf52508600e833e40c143ccc
Tiffany <tiffany.prosser@leicester.gov.uk>
2019-10-10 13:57:34 +0100

    more stuff

---
+++ CogentDemo.UI/App_Plugins/Navigation/navigation.resource.js
@@ -0,0 +1,11 @@
+﻿angular.module("umbraco.resources")
+    .factory("navigationResource", function ($http) {
+        return {
+            getById: function (id) {
+                return $http.get("backoffice/Navigation/NavigationApi/GetById?id=" + id);
+            },
+            publish: function (id) {
+                return $http.post("backoffice/Navigation/NavigationApi/PostPublish?id=" + id);
+            }
+        };
+    });
